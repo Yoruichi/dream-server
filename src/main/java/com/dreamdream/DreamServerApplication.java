aa9ad4a164e121f937bd81055ccb5db8166400d9
yoruichi <hz-zhangcong@163.com>
2016-10-03 16:19:16 +0800

    init project

---
+++ src/main/java/com/dreamdream/DreamServerApplication.java
@@ -0,0 +1,12 @@
+package com.dreamdream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamServerApplication.class, args);
+	}
+}
